7edde8133de91fddaf449ca848299f013f14bfe6
Matheus Mayan Abbadi <66267966+MMAbbadi@users.noreply.github.com>
2024-09-09 10:09:16 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,22 @@
+let characters = [
+  {
+    name: "Asuka Kazama",
+    description: "Asuka Kazama is a master of Kazama-style traditional martial arts and is a well-known mediator. She once defeated the young heiress Lili at The King of Iron Fist Tournament, but after that victory, Asuka got the feeling she was being followed wherever she went. Lili's increasingly irrational behavior escalated day by day. One day Asuka came home to discover her dojo was bought by Lili and covered in rose decorations",
+    style:  "Kazama-Style Traditional Martial Arts",
+    country: "Japan"
+  },
+
+  {
+    name: "Emilie de Rochfort",
+    description: "Daughter of a wealthy oil magnate from Monaco who, despite her pampered upbringing, loves to fight. Keeping it a secret from her peace-loving father, Lili uses the family private jet to fly around the globe in search of worthy opponents to hone her skills. All of it to defeat her eternal rival, Asuka Kazama, who has beaten her twice in The King of Iron Fist Tournament. Lili buys the Kazama family's dojo with her pocket money, decorates the entire dojo with roses, and sends Asuka a formal challenge. Imagining Asuka's angry face, Lili smiles elegantly while petting her cat, Salt.",
+    style:  "Self-Taught Style",
+    country: "Monaco"
+  },
+
+  {
+    name: "Kazuya Mishima​",
+    description: "Kazuya Mishima used the full extent of his devil power and G Corp to try and take over the world. After defeating his father Heihachi Mishima, Kazuya doubled down on his efforts to take down the leaderless Mishima Zaibatsu. This brutal war divided the world in two, and in the end G Corp came out on top. However, the conglomerate showed no signs of relenting, and continued to dominate the world with an iron fist.",
+    style: "Mishima-Style Fighting Karate​",
+    country: "None (Relinquished his Japanese nationality)"
+  }
+]
